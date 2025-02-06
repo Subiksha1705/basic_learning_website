76ca1321879880353d58317e44ef830c56a20434
Subiksha <124233005+Subiksha1705@users.noreply.github.com>
2025-02-06 13:27:44 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-    echo hi123
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker login -u subiksha17 -p s1705sha17
-    docker tag learning_web subiksha17/myreact
-    docker push subiksha17/myreact
+echo "hi123"
+sh 'chmod +x build.sh'
+sh './build.sh'
+docker login -u subiksha17 -p s1705sha17
+docker tag learning_web subiksha17/myreact
+docker push subiksha17/myreact
+
+# Run the Docker container
+docker run -d -p 80:80 --name myreact_container subiksha17/myreact
