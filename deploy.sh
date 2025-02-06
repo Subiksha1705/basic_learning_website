548d76681faaa587e3417de4d10955ff7143f933
Subiksha <124233005+Subiksha1705@users.noreply.github.com>
2025-02-06 15:21:07 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,6 @@ echo "s1705sha17" | docker login -u subiksha17 --password-stdin
 docker tag learning_web subiksha17/myreact
 docker push subiksha17/myreact
 
-# Ensure Minikube is running before deployment
-if ! minikube status | grep -q "Running"; then
-    echo "Minikube is not running! Starting Minikube..."
-    minikube start
-fi
-
-kubectl config use-context minikube
-
 # Deploy to Kubernetes
 kubectl create deployment myreact-app --image=subiksha17/myreact --port 80
 kubectl expose deployment myreact-app --type=NodePort --port=80
