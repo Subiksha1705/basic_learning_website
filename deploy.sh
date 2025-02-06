ebe219d97fc9683cb42558fa3adc60aa7638baa9
Subiksha <124233005+Subiksha1705@users.noreply.github.com>
2025-02-06 11:25:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u subiksha17 -p s1705sha17
-    docker tag react_app subiksha17/myreact
+    docker tag learning_web subiksha17/myreact
     docker push subiksha17/myreact
